2fbf2ac6949c9007f2cf79c154e2f8e339e9cbe3
MetteMunch <meha0005@stud.kea.dk>
2025-11-07 09:55:13 +0100

    Initial commit tilknytning til GitHub repo

---
+++ src/main/java/mmh/valgfagsfordeling/ValgfagsfordelingApplication.java
@@ -0,0 +1,13 @@
+package mmh.valgfagsfordeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValgfagsfordelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValgfagsfordelingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mmh/valgfagsfordeling/ValgfagsfordelingApplicationTests.java
@@ -0,0 +1,13 @@
+package mmh.valgfagsfordeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValgfagsfordelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
